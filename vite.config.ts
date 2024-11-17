5484311baf5b7c51a4f6b79b45c90fcbd62c6c3f
Nizam Salihin <nizamsalihin14@gmail.com>
2024-11-17 10:01:33 +0700

    add basic sidebar

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			// allowing serving files from one level up to the project roo
+			allow: ['..']
+		}
+	}
 });
